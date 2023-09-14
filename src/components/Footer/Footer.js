75e7f07afe11009537066fc096fd343966f57ddb
Mark Fady <markfady9@gmail.com>
2023-09-14 22:12:46 +0300

    Footer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,16 @@
+import './Footer.css'
+
+export default function Footer() {
+  return (
+    <>
+        <div className="footer pt-3">
+            <div className="container">
+                <div className="text">
+                    <p>Copyright © 2022 Mexant Co., Ltd. All Rights Reserved.</p>
+                    <p>Designed by <span className='active'>TemplateMo</span></p>
+                </div>
+            </div>
+        </div>
+    </>
+  )
+}
